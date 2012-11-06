c4552222d8e9a190b71489bfca4152ba8f58643b
Christoph Laaber <christoph.laaber@gmail.com>
2012-11-06 18:20:03 +0100

    set up cucumber, rspec; created home page (same layout as provided from simon); startimg point for us

---
+++ game_archive/spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :user do
+    name 'Test User'
+    email 'example@example.com'
+    password 'password'
+    password_confirmation 'password'
+    #required if the Devise Confirmable module is used
+    #confirmed_at Time.now
+  end
+end
