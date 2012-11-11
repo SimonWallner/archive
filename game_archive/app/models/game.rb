1cddd8de18fbded2bbf982f38b52f616bfb53241
Christoph Laaber <christoph.laaber@gmail.com>
2012-11-11 18:48:56 +0100

    added join table game_genres as well as the association between game and genre

--- game_archive/app/models/game.rb
+++ game_archive/app/models/game.rb
@@ -1,4 +1,6 @@
 class Game < ActiveRecord::Base
   attr_accessible :description, :title
   validates :title, :presence => true
+
+  has_and_belongs_to_many :genres
 end
--- game_archive/db/schema.rb
+++ game_archive/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121110152806) do
+ActiveRecord::Schema.define(:version => 20121111173526) do
 
   create_table "developers", :force => true do |t|
     t.string   "name"
@@ -25,6 +25,11 @@ ActiveRecord::Schema.define(:version => 20121110152806) do
     t.text     "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
+
+  create_table "games_genres", :id => false, :force => true do |t|
+    t.integer "game_id"
+    t.integer "genre_id"
   end
 
   create_table "genres", :force => true do |t|
