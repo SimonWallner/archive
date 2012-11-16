42fd9937d45bee54c974e5b5395255a2e8708af5
Till Riemer <till.riemer@gmx.de>
2012-11-16 15:05:46 +0100

    markdown renderer mit images verboten

--- game_archive/app/helpers/application_helper.rb
+++ game_archive/app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 	def markdown(text)
-		markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
-		:autolink => true, :space_after_headers => true)
+		rndr = Redcarpet::Render::HTML.new(:filter_html => true, :no_images => true, :hard_wrap => true)
+		markdown = Redcarpet::Markdown.new(rndr, :autolink => true, :space_after_headers => true)
 		markdown.render(text).html_safe
 	end
 end
