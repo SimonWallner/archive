b00e22cda84ac068f6f9f866d5d5cf0843029513
snapper <dominikamon@gmail.com>
2012-11-24 11:43:10 +0100

    added uploads folder to .gitignore, fixed error with storedir

--- game_archive/app/uploaders/image_uploader.rb
+++ game_archive/app/uploaders/image_uploader.rb
@@ -17,7 +17,14 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"  # include CarrierWave::MiniMagick
+
+  # Include the Sprockets helpers for Rails 3.1+ asset pipeline compatibility:
+  # include Sprockets::Helpers::RailsHelper
+  # include Sprockets::Helpers::IsolatedHelper
+
+  # Choose what kind of storage to use for this uploader:
+
   end
 
   #Provide a default URL as a default if there hasn't been a file uploaded:
@@ -29,12 +36,14 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   end
 
+
+
   # Process files as they are uploaded:
   # process :scale => [200, 300]
   #
- def scale(width, height)
+ #def scale(width, height)
   #   # do something
- end
+ #end
 
    #Create different versions of your uploaded files:
    version :tiled_2x do
@@ -51,7 +60,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
-     %w(jpg jpeg gif png)
+     %w(jpg jpeg png)
   end
 
   # Override the filename of the uploaded files:
