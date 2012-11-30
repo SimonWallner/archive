9c78d64bbf1a7e803f4f38bb998878afb9e0b5a9
snapper <dominikamon@gmail.com>
2012-11-30 14:23:18 +0100

    tested videoembed

--- game_archive/app/helpers/application_helper.rb
+++ game_archive/app/helpers/application_helper.rb
@@ -7,7 +7,22 @@ module ApplicationHelper
 		markdown = Redcarpet::Markdown.new(rndr, :space_after_headers => true, :autolink => true)
 		markdown.render(text).html_safe
 		
-	end
+  end
+
+  def youtube_embed(youtube_url)
+    if youtube_url[/youtu\.be\/([^\?]*)/]
+      youtube_id = $1
+    else
+      # Regex from # http://stackoverflow.com/questions/3452546/javascript-regex-how-to-get-youtube-video-id-from-url/4811367#4811367
+      youtube_url[/^.*((v\/)|(embed\/)|(watch\?))\??v?=?([^\&\?]*).*/]
+      youtube_id = $5
+    end
+
+    %Q{<iframe title="YouTube video player" width="688" height="349" src="http://www.youtube.com/embed/#{ youtube_id }" frameborder="0" allowfullscreen></iframe>}
+  end
+
+
+
 class LinksInNewWindow < Redcarpet::Render::HTML
     def link(link, title, alt_text)
       "<a target=\"_blank\" href=\"#{link}\">#{alt_text}</a>"
