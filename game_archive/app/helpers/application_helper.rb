3180f30f3e9ff83f5644959afff7c937b496aed7
Christoph Laaber <christoph.laaber@gmail.com>
2012-12-07 12:01:21 +0100

    added just links markdown to markdown fields

--- game_archive/app/helpers/application_helper.rb
+++ game_archive/app/helpers/application_helper.rb
@@ -9,33 +9,43 @@ module ApplicationHelper
 		markdown = Redcarpet::Markdown.new(rndr, :space_after_headers => true, :autolink => true)
 		markdown.render(text).html_safe
 		
-	end
-class LinksInNewWindow < Redcarpet::Render::HTML
-    def link(link, title, alt_text)
-      "<a target=\"_blank\" href=\"#{link}\">#{alt_text}</a>"
-    end
-    def autolink(link, link_type)
-      if link[-1] =='/'
-        link=link[0..-1]
+  end
+
+  class LinksInNewWindow < Redcarpet::Render::HTML
+      def link(link, title, alt_text)
+        "<a target=\"_blank\" href=\"#{link}\">#{alt_text}</a>"
       end
-      link_title= link.split("/")
-      link_end =""
-      if link_title.length>5
-        link_title=link_title[0..4]
-        link_end = "/..."
-      end
-        link_t = link_title[2]
-      for i in 3..link_title.length-1
-        if link_title[i].length>20
-          link_end = "/..."
-          break
+      def autolink(link, link_type)
+        if link[-1] =='/'
+          link=link[0..-1]
         end
-        link_t += "/" + link_title[i]
+        link_title= link.split("/")
+        link_end =""
+        if link_title.length>5
+          link_title=link_title[0..4]
+          link_end = "/..."
+        end
+          link_t = link_title[2]
+        for i in 3..link_title.length-1
+          if link_title[i].length>20
+            link_end = "/..."
+            break
+          end
+          link_t += "/" + link_title[i]
+        end
+          link_t +=link_end
+        "<a target=\"_blank\" href=\"#{link}\">#{link_t}</a>"
       end
-        link_t +=link_end
-      "<a target=\"_blank\" href=\"#{link}\">#{link_t}</a>"
-    end
-end
+  end
+
+  def just_links_markdown(text)
+    logger.debug "render just links"
+    logger.debug text
+    renderer = JustLinksRenderer.new :filter_html => true
+    logger.debug "renderer initialised"
+    markdown = Redcarpet::Markdown.new renderer, :autolink => true
+    markdown.render(text).html_safe
+  end
 
   # returns an array of MixedField Objects which fit the given type
   # object can be game, company, developer
