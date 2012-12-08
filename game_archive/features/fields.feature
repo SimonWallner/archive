250d3c027c00e514d3c55d64a6e7971b8b354872
yildiz yazicioglu <yildizz83@gmail.com>
2012-12-08 21:34:15 +0100

    .

--- game_archive/features/fields.feature
+++ game_archive/features/fields.feature
@@ -10,19 +10,9 @@ Feature:
   Scenario: add developer which links to a developer
     Given I have a developer DevA
     And I am on the game creation page
-    When I enter valid game data
-    And I add the field Genres
+    When I enter valid game data 
     And I enter a developer field with a developer
     Then The data should be saved
     And I should see the developer field
-
-  @javascript
-  Scenario: insert new genres
-    Given I have a game Tetris
-    And I have a genre named Puzzle
-    And I am on the detail page of the game
-    When I follow the game edit link
-    And I add the field Genres
-    And I input "Pu" in the new_genres_input field
-    And I click the autocomplete for "Puzzle"
-    Then I should have genre "Puzzle" added
+    
+ 
--- game_archive/features/step_definitions/fields_steps.rb
+++ game_archive/features/step_definitions/fields_steps.rb
@@ -1,91 +1,32 @@
-Given /^I have a game$/ do
-  @game_title = "mario"
-  @gameField=FactoryGirl.create :game  
+When /^I enter valid game data$/ do
+  @game_title = "Game1"
+  fill_in "game_title", :with => @game_title
 end
+ 
+ 
 
-When /^I follow the Add Field button$/ do
-  visit new_game_path
-     fill_in "game_title", :with => @game_title
+When /^I enter a developer field with a developer$/ do
   click_link_or_button "Add Field"
-  sleep(1)
-end
-
-Then /^I enter game field with "(.*?)"$/ do |fill|
-
-  within(".newFieldsDiv .addedField") do
-    @day = "21"
-    @month = "3"
-    @year = "2012"
-    if fill == "Release Dates"
-
-      select(fill, :from => 'newFieldId')
-      within(".release_dates_div") do
-        select(@day, :from => 'day_release_date1')
-        select(@month, :from => 'month_release_date1')
-        fill_in "year_release_date1", :with => @year
-        sleep(1)
-      end
-
-    end
-
-    if fill == "Developer"
-      @text = "new developer"
-      select(fill, :from => 'newFieldId')
-      fill_in "new_developers", :with => @text
-      sleep(1) end
-    if fill == "Publisher"
-      @text = "new publisher"
-      select(fill, :from => 'newFieldId')
-      fill_in "new_publishers", :with => @text
-      sleep(1) end
-    if fill == "Distributor"
-      @text = "new distributor"
-      select(fill, :from => 'newFieldId')
-      fill_in "new_distributors", :with => @text
-      sleep(1) end
-    if fill == "Credits"
-      @text = "new credits"
-      select(fill, :from => 'newFieldId')
-      fill_in "new_credits", :with => @text
-      sleep(1) end
-    if fill == "Series"
-      @text = "new series"
-      select(fill, :from => 'newFieldId')
-      fill_in "new_series", :with => @text
-      sleep(1) end
-    if fill == "Genres"
-      @text = "new genres"
-      select(fill, :from => 'newFieldId')
-      fill_in "new_genres_input", :with => @text
-      sleep(1)
-    end
     
+    within(".newFieldsDiv .addedField") do
+       
+      @text = "new developer"
+      select("Developer", :from => 'newFieldId') 
+        fill_in "new_developers", :with => "@dev:#{@givenDeveloper.id}:dev"
+      sleep(1)      
+    end      
+  click_link_or_button "Create Game"
+end
+
  
-  end
-
-
+Then /^The data should be saved$/ do
+  @g = Game.find_by_title @game_title
+  @g.should_not == nil
+  @g.mixed_fields.should_not == nil
+  @g.mixed_fields.size.should == 1
 end
 
-
-  
-  
-  Then /^The data should be saved$/ do
-   
-  g = Game.find_by_title @game_title
-  
-  g.should_not == nil
-   
-  g.mixed_fields.should_not == nil
-  
-  g.mixed_fields.size.should == 1
-  
+Then /^I should see the developer field$/ do
+  page.should have_content(@givenDeveloper.name)
 end
-
-
-Then /^I should see the mixed field type added$/ do
-   #page.should have_content(@day + "." + @month + "." +@year)
-    page.should have_content(@game_title)
-end
-  
  
-
