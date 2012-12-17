c3fde5bbfeec38618efe26d7fae4cddf7cfae27b
Christoph Laaber <christoph.laaber@gmail.com>
2012-12-17 12:42:36 +0100

    fixed rspec bug. tests where failing because of not confirmed users (factory girl)

--- game_archive/spec/support/controller_macros.rb
+++ game_archive/spec/support/controller_macros.rb
@@ -2,7 +2,7 @@ module ControllerMacros
   def login_admin
     before(:each) do
       @request.env["devise.mapping"] = Devise.mappings[:user]
-      admin = FactoryGirl.create(:user)
+      admin = FactoryGirl.create(:confirmed_user)
       admin.toggle! :admin
       sign_in admin
     end
@@ -11,7 +11,7 @@ module ControllerMacros
   def login_user
     before(:each) do
       @request.env["devise.mapping"] = Devise.mappings[:user]
-      user = FactoryGirl.create(:user)
+      user = FactoryGirl.create(:confirmed_user)
       sign_in user
     end
   end
