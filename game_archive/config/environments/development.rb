20ee7116c1133e7c7a276755cd55d0eb32c51021
Christoph Laaber <christoph.laaber@gmail.com>
2013-01-07 12:41:57 +0100

    GA-60: implemented

--- game_archive/config/environments/development.rb
+++ game_archive/config/environments/development.rb
@@ -14,21 +14,21 @@ GameArchive::Application.configure do
   config.action_controller.perform_caching = false
   
   # ActionMailer Config
-  config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
-  port: 587,
-  domain: "localhost:3000",
-  authentication: "plain",
-  enable_starttls_auto: true,
-  user_name: "gamejam.archive.dev@gmail.com",
-  password: "gamejam123"
-  }
+  #config.action_mailer.smtp_settings = {
+  #  address: "smtp.gmail.com",
+  #  port: 587,
+  #  domain: "localhost:3000",
+  #  authentication: "plain",
+  #  enable_starttls_auto: true,
+  #  user_name: "gamejam.archive.dev@gmail.com",
+  #  password: "gamejam123"
+  #}
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   # change to false to prevent email from being sent during development
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = false
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default :charset => "utf-8"
+  #config.action_mailer.default :charset => "utf-8"
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
