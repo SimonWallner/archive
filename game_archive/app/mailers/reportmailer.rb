02054a94cf6133a216ba75f5da53f740c37bc610
Till Riemer <till.riemer@gmx.de>
2013-01-20 17:21:18 +0100

    reportmailer fertig, tests fehlen noch

---
+++ game_archive/app/mailers/reportmailer.rb
@@ -0,0 +1,12 @@
+class Reportmailer < ActionMailer::Base
+  default from: "admin@example.com"
+  
+  def report(ctype, cid, reason, sent_at = Time.now)
+      @subject = "Report content notification"
+      @recipients = User.find_by_admin(true)
+      @from = 'no-reply@example.com'
+      @sent_on = sent_at
+	  mail(:to => @recipients.email, :subject => @subject, :text => "An article of type " + ctype.to_s + " with ID " + cid.to_s + " has been reported for the following reason: " + reason)
+     
+   end
+end
--- game_archive/app/models/reportblockcontent.rb
+++ game_archive/app/models/reportblockcontent.rb
@@ -25,6 +25,9 @@ class Reportblockcontent < ActiveRecord::Base
 			end
 			new_report = Reportblockcontent.new :content_type => ctype, :content_id => cid, :reason => reason, :status => content_status, :email => email, :admin_id => admin_id
 			new_report.save
+			
+			# send notification mail to admin
+			Reportmailer.report(ctype, cid, reason).deliver
 		end
 	end
   end
